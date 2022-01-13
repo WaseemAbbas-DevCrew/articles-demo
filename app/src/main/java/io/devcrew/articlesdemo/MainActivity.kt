b73a9d747a9d2eb5a18fb9d7c4a2eb1b2ec084dd
Waseem Abbas <waseem.abbas@devcrew.io>
2022-01-13 10:19:06 +0500

    initial commit

---
+++ app/src/main/java/io/devcrew/articlesdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package io.devcrew.articlesdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
